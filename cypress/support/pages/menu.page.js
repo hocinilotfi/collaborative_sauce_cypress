47042320c4dc5a2ee954a8af7676bf2703d6dd9c
Briyan Kessel Kouam Fozeu <briyan-kessel.kouam-fozeu@efrei.net>
2025-07-16 15:28:16 +0200

    🎨 Complete menu page creation

--- cypress/support/pages/menu.page.js
+++ cypress/support/pages/menu.page.js
@@ -3,12 +3,21 @@
 export class MenuPage {
 
     get burgerBtn() {
-        return cy.get("#bm-burger-button");
+        return cy.get(".bm-burger-button");
     }
     get closeBtn() {
-        return cy.get("#bm-burger-button");
+        return cy.get(".bm-cross-button");
+    }
+    get logoutLink(){
+        return cy.get("#logout_sidebar_link");
+    }
+    get resetAppStateLink(){
+        return cy.get(".reset_sidebar_link");
     }
 
     open = () => this.burgerBtn.click();
     close = () => this.closeBtn.click();
+
+    logout = () => this.logoutLink.click();
+    emptyCart = () => this.resetAppStateLink.click();
 }
