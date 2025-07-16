449c121bb02e93a47ecc1be79ecea59829d3a75f
sThiziri <soualahthiziri@gmail.com>
2025-07-16 16:35:22 +0200

    ajout page menu

---
+++ cypress/pages/menu.page.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+class Menu {
+    elements = {
+        allItem: ()=>  cy.get("#inventory_sidebar_link"),
+        about: ()=> cy.get("#about_sidebar_link"),
+        logout: ()=> cy.get("#logout_sidebar_link"),
+        reset: ()=> cy.get('#reset_sidebar_link')
+    }
+
+    //actions
+    cliqueMenuAllItem(){
+        this.elements.allItem().click();
+    }
+
+    cliqueMenuAbout(){
+        this.elements.about().click();
+    }
+
+    cliqueLogout(){
+        this.elements.logout().click();
+    }
+
+    cliqueReset(){
+        this.elements.logout().click();
+    }
+
+}
+
+export default new Menu();
