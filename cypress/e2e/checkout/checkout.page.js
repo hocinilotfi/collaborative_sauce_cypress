7854aaf0d78db98fb25209346a90e604308f2774
Eya Ayadi <eyaayedi2001@gmail.com>
2025-07-16 15:08:15 +0200

    Task-007_Page_Checkout

---
+++ cypress/e2e/checkout/checkout.page.js
@@ -0,0 +1,19 @@
+  
+ class CheckoutPage { 
+  
+  
+  
+  
+  startCheckout(firstName, lastName, postalCode) {
+    cy.get('[data-test="checkout"]').click();
+    cy.get('[data-test="firstName"]').type(firstName);
+    cy.get('[data-test="lastName"]').type(lastName);
+    cy.get('[data-test="postalCode"]').type(postalCode);
+    cy.get('[data-test="continue"]').click();
+    cy.get('[data-test="finish"]').click();
+  }
+
+  verifyCheckoutComplete() {
+    cy.get('.complete-header').should('contain', 'Thank you');
+  }
+}
