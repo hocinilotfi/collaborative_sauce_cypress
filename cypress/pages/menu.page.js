1509a3315985514ca7d9a3133dfe9fe6e3b4fbd4
Briyan Kessel Kouam Fozeu <briyan-kessel.kouam-fozeu@efrei.net>
2025-07-17 15:51:43 +0200

    Revert [ ✅ Add About is visible test]

--- cypress/pages/menu.page.js
+++ cypress/pages/menu.page.js
@@ -2,8 +2,6 @@
 
 class Menu {
     elements = {
-        burgerIcon: ()=> cy.get("#react-burger-menu-btn"),
-        closeIcon: ()=> cy.get("#react-burger-cross-btn"),
         allItem: ()=>  cy.get("#inventory_sidebar_link"),
         about: ()=> cy.get("#about_sidebar_link"),
         logout: ()=> cy.get("#logout_sidebar_link"),
