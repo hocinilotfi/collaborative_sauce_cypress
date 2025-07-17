4cd10cf872e697bb2950f1f0b442ff84a91a2f15
Gael NOKAM NOKAM <nokamnokamgael@gmail.com>
2025-07-17 15:02:45 +0200

    ajout et suppression des articles dans le panier

--- cypress/e2e/panier.cy.js
+++ cypress/e2e/panier.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import { panierPage } from '../page/panier.page'
+import { panierPage } from '../pages/panier.page.js'
 
 describe('Tests sur la page Panier', () => {
 
