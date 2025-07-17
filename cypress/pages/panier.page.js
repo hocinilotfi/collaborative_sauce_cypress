7d9479114d37cdf9a24fe9020276b6e4e1dea4ba
sThiziri <soualahthiziri@gmail.com>
2025-07-17 14:19:44 +0200

    TASK-006 : ajout des actions à la page panier

--- cypress/pages/panier.page.js
+++ cypress/pages/panier.page.js
@@ -4,16 +4,49 @@ class PanierPage{
     //locators
     elements = {
         remonveBackpackElement: ()=>  cy.get("#remove-sauce-labs-backpack"),
-        remonveBikeLightElement: ()=>  cy.get("#remove-sauce-labs-bike-light"),
-        remonveBoltTshirtElement: ()=>  cy.get("#remove-sauce-labs-bolt-t-shirt"),
-        remonveFleeceJacketElement: ()=>  cy.get("#remove-sauce-labs-fleece-jacket"),
-        remonveOnsieElement: ()=>  cy.get("#remove-sauce-labs-onesie"),
-        remonveTestAllTheThingsElement: ()=>  cy.get("#remove-test.allthethings()-t-shirt-(red)"),
+        removeBikeLightElement: ()=>  cy.get("#remove-sauce-labs-bike-light"),
+        removeBoltTshirtElement: ()=>  cy.get("#remove-sauce-labs-bolt-t-shirt"),
+        removeFleeceJacketElement: ()=>  cy.get("#remove-sauce-labs-fleece-jacket"),
+        removeOnsieElement: ()=>  cy.get("#remove-sauce-labs-onesie"),
+        removeTestAllTheThingsElement: ()=>  cy.get("#remove-test.allthethings()-t-shirt-(red)"),
 
         checkoutElement: ()=> cy.get("#checkout"),
         continueShoppingElement: ()=>  cy.get("#continue-shopping"),
 
     }
 
+    //actions 
+
+    remonveBackpack(){
+        this.elements.remonveBackpackElement().click();
+    }
+
+    removeBikeLight(){
+        this.elements.removeBikeLightElement().click();
+    }
+
+    removeBoltTshirt(){
+        this.elements.removeBoltTshirtElement().click();
+    }
+
+    removeFleeceJacket(){
+        this.elements.removeFleeceJacketElement().click();
+    }
+
+    removeOnsie(){
+        this.elements.removeOnsieElement().click();
+    }
+
+    removeTestAllTheThings(){
+        this.elements.removeTestAllTheThingsElement().click();
+    }
+
+    continueShopping(){
+        this.elements.continueShoppingElement().click();
+    }
+
+    checkout(){
+        this.elements.checkoutElement().click();
+    }
 
 }
