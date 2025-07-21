8104aab08d3c1d8b37d41cefbf84f8f9a5b7010f
Juba027 <jubaamara9852@gmail.com>
2025-07-21 15:14:31 +0300

    TASK/TC-005 test resilient

--- cypress/e2e/panier.cy.js
+++ cypress/e2e/panier.cy.js
@@ -3,41 +3,44 @@ import dashboardPage from '../pages/dashboard.page';
 import loginPage from '../pages/login.page';
 import { panierPage } from '../pages/panier.page';
 
-describe ('Verifier l ajout ou la suppression d un produit dans le panier',{tags:['@TC-005','@Panier','@Regression']}, ()=> {
+describe ('Verifier l ajout ou la suppression d un produit dans le panier',{tags:['@tc-005','@panier','@regression']}, ()=> {
+    const indexProduit = 1;
+    let nomProduit = '';
     beforeEach (()=>{
         cy.visit('https://www.saucedemo.com/');
         loginPage.doLogin('standard_user','secret_sauce');
 
         
-
+    dashboardPage.elements.productItems().eq(indexProduit)
+      .find('.inventory_item_name')
+      .invoke('text')
+      .then((text) => {
+        nomProduit = text.trim();
+      });
     });
-    it('Ajout du produit et verifie qu il est bien ajoutee ', {tags : ['@Ajout','@TC-005']}, () =>{
-        dashboardPage.elements.productItems().then((items) => {
-            const randomIndex = Math.floor(Math.random() * items.length);
-            const selectedItem = items[randomIndex];
-
-            cy.wrap(selectedItem).find('.inventory_item_name').invoke('text').then((nomProduit) => {
-                
-            })
-        })
-        dashboardPage.ajouterProduitAuPanier('sauce-labs-backpack');
+    it('Ajout du produit et verifie qu il est bien ajoutee ', {tags : ['@ajout','@tc-005-01']}, () =>{
+        
+        dashboardPage.ajouterProduitAuPanier(indexProduit);
         dashboardPage.elements.cartNumber().should('have.text','1');
 
         dashboardPage.allerAuPanier();
-        panierPage.elements.removeBackpackElement().should('be.visible');
-
+        cy.get('.inventory_item_name').should('contain.text', nomProduit);
     })
-    it('Supprime le produit du panier et verifie qu il est retiree', {tags : ['@Supression','@TC-005'] },() => {
-        dashboardPage.ajouterProduitAuPanier('sauce-labs-backpack')
+    it('Supprime le produit du panier et verifie qu il est retiree', {tags : ['@supression','@tc-006-02'] },() => {
+        dashboardPage.ajouterProduitAuPanier(indexProduit);
         dashboardPage.allerAuPanier();
-        panierPage.elements.removeBackpackElement().should('be.visible');
+        cy.get('.cart_item .inventory_item_name').should('contain.text', nomProduit);
 
-        panierPage.removeBackpack();
+        cy.get('.cart_item')
+        .contains(nomProduit)
+        .parents('.cart_item')
+        .find('button')
+        .click();
 
-        panierPage.elements.removeBackpackElement().should('not.exist');
+        cy.get('.cart_item .inventory_item_name').should('not.exist');
+        dashboardPage.elements.cartNumber().should('not.exist');
 
-        dashboardPage.elements.cartNumber().should('not.exist')
-    })
+    });
 
 
     
